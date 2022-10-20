6f8791024f83d1558597a33e7991f908dc9684ba
qpakr38 <zczc0819@gmail.com>
2022-10-20 15:01:08 +0900

    Project Init

---
+++ src/test/java/com/toyproject/springportfolio/SpringPortFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.springportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPortFolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
